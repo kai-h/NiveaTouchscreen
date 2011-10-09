36089b574356afe2800487a5c9395f27c08ae40b
Kai Howells <kai@automatica.com.au>
2011-10-09 22:26:22 +1100

    Main nav done.
    Main nav and buttons done. Working on the products

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /Users/kai/Documents/Client\ Documents/Popcorn/nivea\ touchscreen
-rm unencrypted.zip
-rm kiosk.zip
-zip unencrypted.zip *
-openssl enc -des3 -salt -pass pass:gAS3xfcb67 -in unencrypted.zip -out kiosk.zip
-cp kiosk.zip /Volumes/NO\ NAME/
-diskutil eject /Volumes/NO\ NAME/
-echo "Done."
